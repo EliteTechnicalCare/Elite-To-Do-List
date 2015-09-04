a2df49d7245b02e4a985ec7c106a5756988d851d
David Hakki <david.hakki@elitetechnicalcare.com>
2015-09-04 18:24:58 -0400

    Initial Commit

---
+++ Elite To-Do List/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  Elite To-Do List
+//
+//  Created by David-Daoud Hakki on 9/4/15.
+//  Copyright © 2015 Elite Technical Care LLC. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ Elite To-Do List/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  Elite To-Do List
+//
+//  Created by David-Daoud Hakki on 9/4/15.
+//  Copyright © 2015 Elite Technical Care LLC. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
